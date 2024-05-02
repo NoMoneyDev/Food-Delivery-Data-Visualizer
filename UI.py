d5aa9922a41292ab85954cbeb4bb94f319cb0d59
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-05-02 16:00:17 +0700

    add UI files

---
+++ UI.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import matplotlib as plt
+
+
+class UI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.component_init()
+
+    def component_init(self):
+        pass
+
+    def run(self):
+        self.root.mainloop()
+
