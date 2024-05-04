a1c58ead66e127189ee006bb45b4310d60120394
Rattanan Runguthai <japanrisesgamer@gmail.com>
2024-05-04 19:33:17 +0700

    Bar Graph tab is functional

--- UI.py
+++ UI.py
@@ -5,15 +5,15 @@ import matplotlib as plt
 import seaborn as sns
 from abc import abstractmethod
 from data_manager import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+matplotlib.use('TkAgg')
 
 
 class UI:
     def __init__(self, root):
         self.root = root
         self.root.title("Food Delivery Data Visualizer")
-
         self.component_init()
-
 
     def component_init(self):
         self.menubar = tk.Menu(self.root)
@@ -24,20 +24,39 @@ class UI:
         self.menubar.add_cascade(label="Data Story", command=lambda: self.change_tab('story'))
 
         self.data_tab = Data_Tab(self.root)
+        self.bar_tab = Bar_Tab(self.root)
 
         self.component_install()
+
+        # Defualt tab
+        self.current_tab = self.data_tab
         self.change_tab('data')
 
     def component_install(self):
         self.root.config(menu=self.menubar)
 
     def change_tab(self, tab):
+        self.current_tab.unpack()
         match tab:
             case 'data':
                 print("Changed Tab to 'Data'")
-                self.data_tab.pack_tab()
+                self.current_tab = self.data_tab
+            case 'hist':
+                pass
+            case 'bar':
+                print("Changed Tab to 'Bar Graph'")
+                self.current_tab = self.bar_tab
             case _:
                 pass
+        self.current_tab.pack_tab()
+
+    @property
+    def screenwidth(self):
+        return self.root.winfo_screenwidth()
+
+    @property
+    def screenheight(self):
+        return self.root.winfo_screenheight()
 
     def run(self):
         self.root.mainloop()
@@ -47,6 +66,9 @@ class New_Tab(tk.Frame):
     def __init__(self, root):
         self.root = root
         super().__init__()
+        self.data = Data_Manager()
+        self.component_init()
+        self.component_install()
 
     @abstractmethod
     def component_init(self):
@@ -54,14 +76,16 @@ class New_Tab(tk.Frame):
 
     @abstractmethod
     def pack_tab(self):
-        pass
+        self.pack(fill=tk.BOTH, expand=True)
+
+    @abstractmethod
+    def unpack(self):
+        self.pack_forget()
 
 
 class Data_Tab(New_Tab):
     def __init__(self, root):
-        self.data = Data_Manager()
         super().__init__(root)
-        self.component_init()
         self.grid_config()
 
     def component_init(self):
@@ -77,24 +101,22 @@ class Data_Tab(New_Tab):
         self.quantity_filter_var
         self.quantity_filter_entry = tk.Entry(self.filters_frame, textvariable=self.quantity_filter_var)
 
-    def pack_tab(self):
-        self.table.pack(fill=tk.BOTH, expand=True)
+    def component_install(self):
+        self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.table_frame.grid(column=0, row=0, sticky=tk.NSEW)
 
         self.filter_text.grid(column=0, row=0, sticky=tk.W)
         self.quantity_filter_text.grid(column=0, row=1, sticky=tk.SW)
         self.quantity_filter_entry.grid(column=0, row=2, sticky=tk.NW)
-        self.filters_frame.grid(column=1, row=0, sticky=tk.NSEW)
-
-        self.pack(fill=tk.Y, expand=True)
+        self.filters_frame.grid(padx=10, column=1, row=0, sticky=tk.NSEW)
 
     def table_col_config(self):
         for col_num in range(len(self.data.get_cols())):
-            self.table.column('#'+str(col_num+1), minwidth = 70, width=155,  stretch=True)
+            self.table.column('#'+str(col_num+1), minwidth = 65, width=150,  stretch=True)
 
     def grid_config(self):
-        self.columnconfigure(0, weight=3, uniform=True)
-        self.columnconfigure(1, weight=2, uniform=True)
+        self.columnconfigure(0)
+        self.columnconfigure(1)
 
         self.rowconfigure(0,weight=1)
 
@@ -121,6 +143,87 @@ class Data_Tab(New_Tab):
     def insert_data(self):
         for row in self.data.get_rows():
             self.table.insert('', tk.END, values=row)
+
+    def unpack(self):
+        self.pack_forget()
+
+
+class Bar_Tab(New_Tab):
+    def __init__(self, root):
+        super().__init__(root)
+        self.grid_config()
+
+    def component_init(self):
+        self.graph_frame = tk.Frame(self)
+        self.config_frame = tk.Frame(self)
+
+        self.graph_img = FigureCanvasTkAgg(self.data.figure, master=self.graph_frame)
+        NavigationToolbar2Tk(self.graph_img, self)
+
+        self.config_text = tk.Label(self.config_frame, text="Config", font=('Arial', 14))
+
+        self.bar_config_text = tk.Label(self.config_frame, text="Bar")
+        self.bar_config_var = tk.StringVar()
+        self.bar_config_combobox = ttk.Combobox(self.config_frame, values=self.data.get_cols(),
+                                                textvariable=self.bar_config_var, state='readonly')
+        self.bar_config_combobox.current(0)
+        self.bar_config_combobox.bind('<<ComboboxSelected>>', self.handle_graph)
+
+        self.height_config_text = tk.Label(self.config_frame, text="Height")
+        self.height_config_var = tk.StringVar()
+        self.height_config_combobox = ttk.Combobox(self.config_frame, values=self.data.get_ordinal_cols(),
+                                                   textvariable=self.height_config_var, state='readonly')
+        self.height_config_combobox.current(1)
+        self.height_config_combobox.bind('<<ComboboxSelected>>', self.handle_graph)
+
+        self.values_config_text = tk.Label(self.config_frame, text="Values")
+        self.values_config_var = tk.StringVar()
+        self.values_config_combobox = ttk.Combobox(self.config_frame, values=['AVERAGE', 'SUM'],
+                                                   textvariable=self.values_config_var, state='readonly')
+        self.values_config_combobox.current(0)
+        self.values_config_combobox.bind('<<ComboboxSelected>>', self.handle_graph)
+
+        self.handle_graph()
+
+    def component_install(self):
+        self.graph_img.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+        self.graph_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.config_text.grid(column=0, row=0, sticky=tk.W)
+        self.bar_config_text.grid(column=0, row=1, sticky=tk.SW, padx=5)
+        self.bar_config_combobox.grid(column=0, row=2, sticky=tk.NW, padx=5)
+        self.height_config_text.grid(column=1, row=1, sticky=tk.SW, padx=5)
+        self.height_config_combobox.grid(column=1, row=2, sticky=tk.NW, padx=5)
+        self.values_config_text.grid(column=2, row=1, sticky=tk.SW, padx=5)
+        self.values_config_combobox.grid(column=2, row=2, sticky=tk.NW, padx=5)
+        self.config_frame.pack(padx=10, side=tk.RIGHT)
+
+    def grid_config(self):
+        self.columnconfigure(0)
+        self.columnconfigure(1)
+
+        self.rowconfigure(0, weight=1)
+
+        self.config_frame.columnconfigure(0, weight=1, uniform=True)
+        self.config_frame.columnconfigure(1, weight=1, uniform=True)
+        self.config_frame.columnconfigure(2, weight=1, uniform=True)
+
+        self.config_frame.rowconfigure(0, weight=1, uniform=True)
+        self.config_frame.rowconfigure(1, weight=1, uniform=True)
+        self.config_frame.rowconfigure(2, weight=1, uniform=True)
+        self.config_frame.rowconfigure(3, weight=1, uniform=True)
+        self.config_frame.rowconfigure(4, weight=1, uniform=True)
+        self.config_frame.rowconfigure(5, weight=1, uniform=True)
+        self.config_frame.rowconfigure(6, weight=1, uniform=True)
+        self.config_frame.rowconfigure(7, weight=1, uniform=True)
+        self.config_frame.rowconfigure(8, weight=1, uniform=True)
+        self.config_frame.rowconfigure(9, weight=1, uniform=True)
+
+    def handle_graph(self, *args):
+        print('called')
+        self.data.bar_graph(self.bar_config_var.get(), self.height_config_var.get(), self.values_config_var.get())
+        self.graph_img.draw()
+        print('plotted')
 
 
 
--- data_manager.py
+++ data_manager.py
@@ -1,13 +1,21 @@
+import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 import numpy as np
+matplotlib.use('TkAgg')
 
 
 class Data_Manager:
     def __init__(self):
         self.__data = pd.read_csv("Orders Data.csv")
+        self.figure = plt.Figure(figsize=(6, 4), dpi=100)
+        self.ax = self.figure.add_subplot()
 
     def get_cols(self):
         return list(self.data.columns)
+
+    def get_ordinal_cols(self):
+        return ['Quantity of Items','Cost','Delivery Time','Food Rating','Delivery Rating']
 
     @property
     def data(self):
@@ -18,3 +26,23 @@ class Data_Manager:
         for row in self.data.itertuples():
             returnlist += [list(row)[1:]]
         return returnlist
+
+    def bar_graph(self, bar, height, val):
+        h_val = []
+        self.ax.clear()
+        df = self.data
+        cols = sorted([col for col in df[bar].unique() if col is not np.nan])
+        match val:
+            case 'SUM':
+                for col in cols:
+                    h_val += [df[df[bar] == col][height].sum()]
+                self.ax.bar(x=cols, height=h_val)
+                self.ax.set_title(f"Sum of {height} to {bar}")
+            case 'AVERAGE':
+                for col in cols:
+                    h_val += [df[df[bar] == col][height].mean()]
+                self.ax.bar(x=cols, height=h_val)
+                self.ax.set_title(f"Average {height} to {bar}")
+        self.ax.set_xlabel(bar)
+        self.ax.set_ylabel(height)
+
